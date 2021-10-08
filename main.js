c275f8e4d3f6a17d0d8cb164019e9b91190ac8e5
VituuD2 <overroot0@gmail.com>
2021-10-07 22:30:47 -0300

    v1

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-let name1 = document.querySelector('input#name')
-let sel1 = document.querySelector('selector#sel1')
-let sel2 = document.querySelector('selector#sel2')
-let radp = document.querySelector('input#p')
-let radg = document.querySelector('input#g')
-let cash = document.querySelector('input#cash')
-let cc = document.querySelector('input#cc')
-let send1 = document.querySelector('input#add')
+let name1 = document.getElementById('name')
+let sel1 = document.getElementById('sel1')
+let sel2 = document.getElementById('sel2')
+let radp = document.getElementById('p')
+let radg = document.getElementById('g')
+let cash = document.getElementById('cash')
+let cc = document.getElementById('cc')
+let send1 = document.getElementById('add')
 
 function send() {
-    if(name1.value || sel1.value || sel2.value == 0){
+    if(name1.value.length == 0 ){
         alert('[ERROR] Insira um valor valido!')
     }else {
-        alert('Everything ok!')
+        let div = document.createElement('div')
+        div.id = 'div1'
+        div.className = 'div1'
+        document.body.appendChild(div)
+        div.innerHTML = sel1.value, '/n'
+        div.innerHTML = name1.value
+
     }
 }
